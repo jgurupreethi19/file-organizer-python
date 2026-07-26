55d9fe24899402b89c02308c73c3f8c5f6a8c836
J GURUPREETHI <gurupreethi628@gmail.com>
2026-07-26 12:55:45 +0530

    Update file_organizer.py

--- file_organizer.py
+++ file_organizer.py
@@ -10,14 +10,14 @@ def move_file(file_path,file,folder,value):
     
                 
 print("Enter the folder path: ")
-count={"Images":0,"Documents":0,"Videos":0,"Audio":0,"Java":0,"Python":0,"Archives":0,"Others":0}
+count={"Images":0,"Documents":0,"Videos":0,"Audio":0,"Python":0,"Java":0,"Archives":0,"Others":0}
 file_path=input()
 if os.path.exists(file_path)==False:
     print("Folder not found")
 else:
     files=os.listdir(file_path)
     d={".jpg":"Images",".jpeg":"Images",".png":"Images",".pdf":"Documents",".docx":"Documents",
-       ".pptx":"Documents",".mp4":"Videos",".mp3":"audio",".py":"Python",".java":"Java",".zip":"Archives",
+       ".pptx":"Documents",".mp4":"Videos",".mp3":"Audio",".py":"Python",".java":"Java",".zip":"Archives",
        ".rar":"Archives"}
     for file in files:
         if os.path.isfile(os.path.join(file_path,file)):
@@ -30,6 +30,7 @@ else:
                 folder=os.path.join(file_path,value)
                 move_file(file_path,file,folder,value)
             else:
+                Others+=1
                 folder=os.path.join(file_path,"Others")
                 if os.path.exists(folder):
                          shutil.move(os.path.join(file_path,file),os.path.join(folder,file))
@@ -38,8 +39,15 @@ else:
                         shutil.move(os.path.join(file_path,file),os.path.join(folder,file))
 
 print("================ SUMMARY ================")                   
-for key in count:
-    print(key,"       : ",count[key])
+print("Images   : ",count["Images"])
+print("Documents: ",count["Documents"])
+print("Videos   : ",count["Videos"])
+print("Audio    : ",count["Audio"])
+print("Python   : ",count["Python"])
+print("Java     : ",count["Java"])
+print("Archives : ",count["Archives"])
+print("Others   : ",count["Others"])
+
 
                 
                 
